06ed8f334847c74477247a72f1c9cc35c01d4762
Biswagit2022 <biswajitprusty18@gmail.com>
2022-06-29 13:11:58 +0530

    Change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello i am Biswajit Prusty from Odisha.Now i am living in Hyderabad and createed some new friends Dileep,kalki and Chakri !'));
+app.get('/', (req, res) => res.send('I am Biswajit Prusty from Odisha.Now i am living in Hyderabad and createed some new friends Dileep,kalki and Chakri !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
