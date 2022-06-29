cc9c9196b27b3e3d2e620fd9e11df84ab3a7faa1
Biswagit2022 <biswajitprusty18@gmail.com>
2022-06-29 12:53:58 +0530

    changes

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-Now i am living in Hyderabad and learning DevOps Course.
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => res.send('Hello i am Biswajit Prusty from Odisha!'));
+
+app.listen(port);
+console.log(`App running on http://localhost:${port}`);
